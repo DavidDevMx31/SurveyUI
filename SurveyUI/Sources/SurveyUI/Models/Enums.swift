ede582ce10bff2937d39723184c3da555ed5085f
David Mendoza <davidc.mendoza31@gmail.com>
2025-05-02 16:18:47 -0600

    Adds SurveyError definition

--- SurveyUI/Sources/SurveyUI/Models/Enums.swift
+++ SurveyUI/Sources/SurveyUI/Models/Enums.swift
@@ -41,3 +41,39 @@ extension QuestionError {
         }
     }
 }
+
+//MARK: Survey errors
+enum SurveyError: LocalizedError {
+    case textIsEmpty
+    case noSelection
+}
+
+extension SurveyError {
+    var failureReason: String? {
+        switch self {
+        case .textIsEmpty:
+            return "El texto de comentarios está vacío"
+        case .noSelection:
+            return "No has seleccionado ninguna opción"
+        }
+    }
+    
+    var errorDescription: String? {
+        switch self {
+        case .textIsEmpty:
+            return "El texto de comentarios es requerido"
+        case .noSelection:
+            return "Seleccionar una opción es requerido"
+        }
+    }
+    
+    var recoverySuggestion: String? {
+        switch self {
+        case .textIsEmpty:
+            return "Agrega tus comentarios"
+        case .noSelection:
+            return "Selecciona una opción"
+        }
+    }
+}
+
