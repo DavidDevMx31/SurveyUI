e30a678552fc238b4925fbfb86a005015c08f3c9
David Mendoza <davidc.mendoza31@gmail.com>
2025-05-02 15:36:25 -0600

    Adds SurveyIntroView

---
+++ SurveyUI/Sources/SurveyUI/Modules/Main/Views/SurveyIntroView.swift
@@ -0,0 +1,23 @@
+//
+//  SurveyIntroView.swift
+//  SurveyUI
+//
+//  Created by David Mendoza on 02/05/25.
+//
+
+import SwiftUI
+
+struct SurveyIntroView: View {
+    let introText: AttributedString
+    
+    var body: some View {
+        Text(introText)
+            .layoutPriority(1)
+            .font(.title)
+            .lineLimit(nil)
+            .multilineTextAlignment(.center)
+    }
+}
+#Preview {
+    SurveyIntroView(introText: AttributedString("Texto de introducción de la encuesta."))
+}
