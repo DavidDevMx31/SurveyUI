2601b5f598c83fba7f71413a2ac4f6fda1c6d160
David Mendoza <davidc.mendoza31@gmail.com>
2025-05-05 16:52:39 -0600

    Defines protocol to select among question options

---
+++ SurveyUI/Sources/SurveyUI/Modules/Main/Protocols/MultipleOptionQuestionStrategy.swift
@@ -0,0 +1,56 @@
+//
+//  MultipleOptionQuestionStrategy.swift
+//  SurveyUI
+//
+//  Created by David Mendoza on 05/05/25.
+//
+
+import Foundation
+
+//MARK: MultipleOptionQuestionStrategy declaration
+protocol MultipleOptionQuestionStrategy {
+    var currentQuestion: Question { get }
+    func selectOption(withId id: String) -> QuestionResult
+}
+
+extension MultipleOptionQuestionStrategy {
+    
+    func limitComment(_ comment: String, maxChars: Int = 100) -> String {
+        String(comment.trimmingCharacters(in: .whitespacesAndNewlines).prefix(100))
+    }
+    
+    func addComment(_ comment: String, withOptionIds selectedIds: [String]?) -> Result<QuestionResult, SurveyError> {
+        guard let allowsTextOption = currentQuestion.allowTextOption else {
+            return .failure(SurveyError.textIsEmpty)
+        }
+        
+        let userComment = limitComment(comment)
+        guard !userComment.isEmpty else {
+            return .failure(SurveyError.textIsEmpty)
+        }
+
+        var newResponse = QuestionResult(questionId: currentQuestion.id)
+        if let selectedOptions = selectedIds {
+            if selectedOptions.contains(allowsTextOption.id) {
+                newResponse.setResponse(selectedOptionsId: selectedOptions, comments: userComment)
+                return .success(newResponse)
+            }
+        }
+        return .failure(SurveyError.noSelection)
+    }
+}
+
+//MARK: Single selection
+struct SingleSelectionStrategy: MultipleOptionQuestionStrategy {
+    private(set) var currentQuestion: Question
+    
+    init(currentQuestion: Question) {
+        self.currentQuestion = currentQuestion
+    }
+    
+    func selectOption(withId id: String) -> QuestionResult {
+        return QuestionResult(questionId: currentQuestion.id, selectedId: id)
+    }
+    
+}
+
